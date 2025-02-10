1d981a53b995258b5fad44e32bba00d672e90abc
NuriaHunt <nuriamartin32@gmail.com>
2025-02-10 17:03:58 +0100

    Practica estaciones

--- python06numeromayor.py
+++ python06numeromayor.py
@@ -1,8 +1,8 @@
 print("numero mayor de dos numeros")
 #preguntamos al usuario dos numeros y mostramos que numero es mayor 
 print("Introduzca numero 1")
-      numero1 = int(input())
-      print("introduzca numero 2")
-      numero2= int(inpout():
+numero1 = int(input())
+print("introduzca numero 2")
+numero2= int(input()):
 if (numero1> numero2):
-      print("el numero "+ str(numero1)
+      print("el numero "+ str(numero1))
---
+++ python08practica.py
@@ -0,0 +1,11 @@
+print ("Estación del usuario")
+numero= 4
+
+if(numero== 1):
+    print("Primavera")
+if(numero== 2):
+    print("Verano")
+if(numero== 3):
+    print("Otoño")
+if(numero== 4):
+    print("Invierno")
